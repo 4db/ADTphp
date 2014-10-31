02418018a02757b0d1790a677cb01bfa3f22744e
alexsdbk <alexs.dbk@gmail.com>
2014-10-30 23:01:03 -0700

    start develop linekdlist

---
+++ LinkedList.php
@@ -0,0 +1,22 @@
+<?php
+
+class Node
+{
+	/* Data to hold */
+	public $data;
+
+	/* Link to next node */
+	public $next;
+
+	/* Node constructor */
+	function __construct($data)
+	{
+		$this->data = $data;
+		$this->next = NULL;
+	}
+
+	function readNode()
+	{
+		return $this->data;
+	}
+}
