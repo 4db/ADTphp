f0ae5329bd082e717c660f7b1bf41e512026c6cd
alexsdbk <alexs.dbk@gmail.com>
2014-11-02 21:34:45 -0800

    do work version linkedlist

--- LinkedList.php
+++ LinkedList.php
@@ -1,22 +1,50 @@
 <?php
 
-class Node
-{
-	/* Data to hold */
-	public $data;
-
+class Node {
 	/* Link to next node */
 	public $next;
+	public $val;
 
 	/* Node constructor */
-	function __construct($data)
+	function __construct($next, $val)
 	{
-		$this->data = $data;
-		$this->next = NULL;
-	}
-
-	function readNode()
-	{
-		return $this->data;
+		$this->next = $next;
+		$this->val = $val;
 	}
 }
+
+class LinkedList {
+	public $count = 0;
+	public $list;
+
+	public function append($value=''){
+		$this->count++;
+
+		if (is_object($this->list )) {
+			$this->list = new Node($this->list, $value);
+		}
+		else {
+			$firstNode = $this->list = new Node($this, $value);
+		}
+	}
+
+	function get($index) {
+		$cur = $this->list;
+		for ($i=0;$i<$index;$i++) {
+			$cur = $cur->next;
+		}
+		return $cur->val;
+	}
+}
+
+$l = new LinkedList();
+$l->append(4);
+$l->append(5);
+$l->append(6);
+$i =0;
+while ($i <= 10) {
+	$l->append($i);
+	$i++;
+}
+var_dump($l->get(3));
+it's work!!!
